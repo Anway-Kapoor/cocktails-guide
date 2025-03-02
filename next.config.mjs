b3bcf8a1f9e2cd21ea54c64f892c4835e63309cd
Anway Kapoor <anwaykapoor2311@gmail.com>
2025-03-03 00:40:44 +0530

    fix error

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
             key: "Content-Security-Policy",
             value: "frame-src 'self' https://www.youtube.com https://www.youtube-nocookie.com https://*.youtu.be; connect-src 'self' https://www.thecocktaildb.com https://www.youtube.com https://www.googleapis.com; img-src 'self' data: https://* http://*; object-src 'none';",
           },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+          },
         ],
       },
     ];
